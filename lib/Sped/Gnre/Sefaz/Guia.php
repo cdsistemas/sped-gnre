c293c6e281759e16377bd428a8d7e7343ce4c301
Victor Vilella <victor.cbl@gmail.com>
2021-08-05 10:01:25 -0300

    Getters e Setters de Guia

--- lib/Sped/Gnre/Sefaz/Guia.php
+++ lib/Sped/Gnre/Sefaz/Guia.php
@@ -45,6 +45,694 @@ class Guia
     private $c01_UfFavorecida;
 
     /**
+     * @return string
+     */
+    public function getC01UfFavorecida()
+    {
+        return $this->c01_UfFavorecida;
+    }
+
+    /**
+     * @param string $c01_UfFavorecida
+     */
+    public function setC01UfFavorecida($c01_UfFavorecida)
+    {
+        $this->c01_UfFavorecida = $c01_UfFavorecida;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC02Receita()
+    {
+        return $this->c02_receita;
+    }
+
+    /**
+     * @param int $c02_receita
+     */
+    public function setC02Receita($c02_receita)
+    {
+        $this->c02_receita = $c02_receita;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC25DetalhamentoReceita()
+    {
+        return $this->c25_detalhamentoReceita;
+    }
+
+    /**
+     * @param int $c25_detalhamentoReceita
+     */
+    public function setC25DetalhamentoReceita($c25_detalhamentoReceita)
+    {
+        $this->c25_detalhamentoReceita = $c25_detalhamentoReceita;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC26Produto()
+    {
+        return $this->c26_produto;
+    }
+
+    /**
+     * @param int $c26_produto
+     */
+    public function setC26Produto($c26_produto)
+    {
+        $this->c26_produto = $c26_produto;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC27TipoIdentificacaoEmitente()
+    {
+        return $this->c27_tipoIdentificacaoEmitente;
+    }
+
+    /**
+     * @param int $c27_tipoIdentificacaoEmitente
+     */
+    public function setC27TipoIdentificacaoEmitente($c27_tipoIdentificacaoEmitente)
+    {
+        $this->c27_tipoIdentificacaoEmitente = $c27_tipoIdentificacaoEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC03IdContribuinteEmitente()
+    {
+        return $this->c03_idContribuinteEmitente;
+    }
+
+    /**
+     * @param int $c03_idContribuinteEmitente
+     */
+    public function setC03IdContribuinteEmitente($c03_idContribuinteEmitente)
+    {
+        $this->c03_idContribuinteEmitente = $c03_idContribuinteEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC28TipoDocOrigem()
+    {
+        return $this->c28_tipoDocOrigem;
+    }
+
+    /**
+     * @param int $c28_tipoDocOrigem
+     */
+    public function setC28TipoDocOrigem($c28_tipoDocOrigem)
+    {
+        $this->c28_tipoDocOrigem = $c28_tipoDocOrigem;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC04DocOrigem()
+    {
+        return $this->c04_docOrigem;
+    }
+
+    /**
+     * @param int $c04_docOrigem
+     */
+    public function setC04DocOrigem($c04_docOrigem)
+    {
+        $this->c04_docOrigem = $c04_docOrigem;
+    }
+
+    /**
+     * @return float
+     */
+    public function getC06ValorPrincipal()
+    {
+        return $this->c06_valorPrincipal;
+    }
+
+    /**
+     * @param float $c06_valorPrincipal
+     */
+    public function setC06ValorPrincipal($c06_valorPrincipal)
+    {
+        $this->c06_valorPrincipal = $c06_valorPrincipal;
+    }
+
+    /**
+     * @return float
+     */
+    public function getC10ValorTotal()
+    {
+        return $this->c10_valorTotal;
+    }
+
+    /**
+     * @param float $c10_valorTotal
+     */
+    public function setC10ValorTotal($c10_valorTotal)
+    {
+        $this->c10_valorTotal = $c10_valorTotal;
+    }
+
+    /**
+     * @return string
+     */
+    public function getC14DataVencimento()
+    {
+        return $this->c14_dataVencimento;
+    }
+
+    /**
+     * @param string $c14_dataVencimento
+     */
+    public function setC14DataVencimento($c14_dataVencimento)
+    {
+        $this->c14_dataVencimento = $c14_dataVencimento;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC15Convenio()
+    {
+        return $this->c15_convenio;
+    }
+
+    /**
+     * @param int $c15_convenio
+     */
+    public function setC15Convenio($c15_convenio)
+    {
+        $this->c15_convenio = $c15_convenio;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC16RazaoSocialEmitente()
+    {
+        return $this->c16_razaoSocialEmitente;
+    }
+
+    /**
+     * @param int $c16_razaoSocialEmitente
+     */
+    public function setC16RazaoSocialEmitente($c16_razaoSocialEmitente)
+    {
+        $this->c16_razaoSocialEmitente = $c16_razaoSocialEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC17InscricaoEstadualEmitente()
+    {
+        return $this->c17_inscricaoEstadualEmitente;
+    }
+
+    /**
+     * @param int $c17_inscricaoEstadualEmitente
+     */
+    public function setC17InscricaoEstadualEmitente($c17_inscricaoEstadualEmitente)
+    {
+        $this->c17_inscricaoEstadualEmitente = $c17_inscricaoEstadualEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC18EnderecoEmitente()
+    {
+        return $this->c18_enderecoEmitente;
+    }
+
+    /**
+     * @param int $c18_enderecoEmitente
+     */
+    public function setC18EnderecoEmitente($c18_enderecoEmitente)
+    {
+        $this->c18_enderecoEmitente = $c18_enderecoEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC19MunicipioEmitente()
+    {
+        return $this->c19_municipioEmitente;
+    }
+
+    /**
+     * @param int $c19_municipioEmitente
+     */
+    public function setC19MunicipioEmitente($c19_municipioEmitente)
+    {
+        $this->c19_municipioEmitente = $c19_municipioEmitente;
+    }
+
+    /**
+     * @return string
+     */
+    public function getC20UfEnderecoEmitente()
+    {
+        return $this->c20_ufEnderecoEmitente;
+    }
+
+    /**
+     * @param string $c20_ufEnderecoEmitente
+     */
+    public function setC20UfEnderecoEmitente($c20_ufEnderecoEmitente)
+    {
+        $this->c20_ufEnderecoEmitente = $c20_ufEnderecoEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC21CepEmitente()
+    {
+        return $this->c21_cepEmitente;
+    }
+
+    /**
+     * @param int $c21_cepEmitente
+     */
+    public function setC21CepEmitente($c21_cepEmitente)
+    {
+        $this->c21_cepEmitente = $c21_cepEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC22TelefoneEmitente()
+    {
+        return $this->c22_telefoneEmitente;
+    }
+
+    /**
+     * @param int $c22_telefoneEmitente
+     */
+    public function setC22TelefoneEmitente($c22_telefoneEmitente)
+    {
+        $this->c22_telefoneEmitente = $c22_telefoneEmitente;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC34TipoIdentificacaoDestinatario()
+    {
+        return $this->c34_tipoIdentificacaoDestinatario;
+    }
+
+    /**
+     * @param int $c34_tipoIdentificacaoDestinatario
+     */
+    public function setC34TipoIdentificacaoDestinatario($c34_tipoIdentificacaoDestinatario)
+    {
+        $this->c34_tipoIdentificacaoDestinatario = $c34_tipoIdentificacaoDestinatario;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC35IdContribuinteDestinatario()
+    {
+        return $this->c35_idContribuinteDestinatario;
+    }
+
+    /**
+     * @param int $c35_idContribuinteDestinatario
+     */
+    public function setC35IdContribuinteDestinatario($c35_idContribuinteDestinatario)
+    {
+        $this->c35_idContribuinteDestinatario = $c35_idContribuinteDestinatario;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC36InscricaoEstadualDestinatario()
+    {
+        return $this->c36_inscricaoEstadualDestinatario;
+    }
+
+    /**
+     * @param int $c36_inscricaoEstadualDestinatario
+     */
+    public function setC36InscricaoEstadualDestinatario($c36_inscricaoEstadualDestinatario)
+    {
+        $this->c36_inscricaoEstadualDestinatario = $c36_inscricaoEstadualDestinatario;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC37RazaoSocialDestinatario()
+    {
+        return $this->c37_razaoSocialDestinatario;
+    }
+
+    /**
+     * @param int $c37_razaoSocialDestinatario
+     */
+    public function setC37RazaoSocialDestinatario($c37_razaoSocialDestinatario)
+    {
+        $this->c37_razaoSocialDestinatario = $c37_razaoSocialDestinatario;
+    }
+
+    /**
+     * @return int
+     */
+    public function getC38MunicipioDestinatario()
+    {
+        return $this->c38_municipioDestinatario;
+    }
+
+    /**
+     * @param int $c38_municipioDestinatario
+     */
+    public function setC38MunicipioDestinatario($c38_municipioDestinatario)
+    {
+        $this->c38_municipioDestinatario = $c38_municipioDestinatario;
+    }
+
+    /**
+     * @return string
+     */
+    public function getC33DataPagamento()
+    {
+        return $this->c33_dataPagamento;
+    }
+
+    /**
+     * @param string $c33_dataPagamento
+     */
+    public function setC33DataPagamento($c33_dataPagamento)
+    {
+        $this->c33_dataPagamento = $c33_dataPagamento;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPeriodo()
+    {
+        return $this->periodo;
+    }
+
+    /**
+     * @param int $periodo
+     */
+    public function setPeriodo($periodo)
+    {
+        $this->periodo = $periodo;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMes()
+    {
+        return $this->mes;
+    }
+
+    /**
+     * @param int $mes
+     */
+    public function setMes($mes)
+    {
+        $this->mes = $mes;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAno()
+    {
+        return $this->ano;
+    }
+
+    /**
+     * @param int $ano
+     */
+    public function setAno($ano)
+    {
+        $this->ano = $ano;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParcela()
+    {
+        return $this->parcela;
+    }
+
+    /**
+     * @param int $parcela
+     */
+    public function setParcela($parcela)
+    {
+        $this->parcela = $parcela;
+    }
+
+    /**
+     * @return array
+     */
+    public function getC39CamposExtras()
+    {
+        return $this->c39_camposExtras;
+    }
+
+    /**
+     * @param array $c39_camposExtras
+     */
+    public function setC39CamposExtras($c39_camposExtras)
+    {
+        $this->c39_camposExtras = $c39_camposExtras;
+    }
+
+    /**
+     * @return string
+     */
+    public function getC42IdentificadorGuia()
+    {
+        return $this->c42_identificadorGuia;
+    }
+
+    /**
+     * @param string $c42_identificadorGuia
+     */
+    public function setC42IdentificadorGuia($c42_identificadorGuia)
+    {
+        $this->c42_identificadorGuia = $c42_identificadorGuia;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRetornoInformacoesComplementares()
+    {
+        return $this->retornoInformacoesComplementares;
+    }
+
+    /**
+     * @param string $retornoInformacoesComplementares
+     */
+    public function setRetornoInformacoesComplementares($retornoInformacoesComplementares)
+    {
+        $this->retornoInformacoesComplementares = $retornoInformacoesComplementares;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRetornoAtualizacaoMonetaria()
+    {
+        return $this->retornoAtualizacaoMonetaria;
+    }
+
+    /**
+     * @param float $retornoAtualizacaoMonetaria
+     */
+    public function setRetornoAtualizacaoMonetaria($retornoAtualizacaoMonetaria)
+    {
+        $this->retornoAtualizacaoMonetaria = $retornoAtualizacaoMonetaria;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRetornoJuros()
+    {
+        return $this->retornoJuros;
+    }
+
+    /**
+     * @param float $retornoJuros
+     */
+    public function setRetornoJuros($retornoJuros)
+    {
+        $this->retornoJuros = $retornoJuros;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRetornoMulta()
+    {
+        return $this->retornoMulta;
+    }
+
+    /**
+     * @param float $retornoMulta
+     */
+    public function setRetornoMulta($retornoMulta)
+    {
+        $this->retornoMulta = $retornoMulta;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRetornoRepresentacaoNumerica()
+    {
+        return $this->retornoRepresentacaoNumerica;
+    }
+
+    /**
+     * @param int $retornoRepresentacaoNumerica
+     */
+    public function setRetornoRepresentacaoNumerica($retornoRepresentacaoNumerica)
+    {
+        $this->retornoRepresentacaoNumerica = $retornoRepresentacaoNumerica;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRetornoCodigoDeBarras()
+    {
+        return $this->retornoCodigoDeBarras;
+    }
+
+    /**
+     * @param int $retornoCodigoDeBarras
+     */
+    public function setRetornoCodigoDeBarras($retornoCodigoDeBarras)
+    {
+        $this->retornoCodigoDeBarras = $retornoCodigoDeBarras;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRetornoSituacaoGuia()
+    {
+        return $this->retornoSituacaoGuia;
+    }
+
+    /**
+     * @param int $retornoSituacaoGuia
+     */
+    public function setRetornoSituacaoGuia($retornoSituacaoGuia)
+    {
+        $this->retornoSituacaoGuia = $retornoSituacaoGuia;
+    }
+
+    /**
+     * @return type
+     */
+    public function getRetornoSequencialGuia()
+    {
+        return $this->retornoSequencialGuia;
+    }
+
+    /**
+     * @param type $retornoSequencialGuia
+     */
+    public function setRetornoSequencialGuia($retornoSequencialGuia)
+    {
+        $this->retornoSequencialGuia = $retornoSequencialGuia;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRetornoErrosDeValidacaoCampo()
+    {
+        return $this->retornoErrosDeValidacaoCampo;
+    }
+
+    /**
+     * @param string $retornoErrosDeValidacaoCampo
+     */
+    public function setRetornoErrosDeValidacaoCampo($retornoErrosDeValidacaoCampo)
+    {
+        $this->retornoErrosDeValidacaoCampo = $retornoErrosDeValidacaoCampo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRetornoErrosDeValidacaoCodigo()
+    {
+        return $this->retornoErrosDeValidacaoCodigo;
+    }
+
+    /**
+     * @param string $retornoErrosDeValidacaoCodigo
+     */
+    public function setRetornoErrosDeValidacaoCodigo($retornoErrosDeValidacaoCodigo)
+    {
+        $this->retornoErrosDeValidacaoCodigo = $retornoErrosDeValidacaoCodigo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRetornoErrosDeValidacaoDescricao()
+    {
+        return $this->retornoErrosDeValidacaoDescricao;
+    }
+
+    /**
+     * @param string $retornoErrosDeValidacaoDescricao
+     */
+    public function setRetornoErrosDeValidacaoDescricao($retornoErrosDeValidacaoDescricao)
+    {
+        $this->retornoErrosDeValidacaoDescricao = $retornoErrosDeValidacaoDescricao;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRetornoNumeroDeControle()
+    {
+        return $this->retornoNumeroDeControle;
+    }
+
+    /**
+     * @param int $retornoNumeroDeControle
+     */
+    public function setRetornoNumeroDeControle($retornoNumeroDeControle)
+    {
+        $this->retornoNumeroDeControle = $retornoNumeroDeControle;
+    }
+
+    /**
      * Para esse atributo é esperado um dado do tipo inteiro
      * para maiores informações visualizar a documentação oficial do GNRE
      * http://www.gnre.pe.gov.br/gnre/index.html
